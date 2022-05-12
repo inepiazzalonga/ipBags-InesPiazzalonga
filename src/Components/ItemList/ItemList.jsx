cdd915b76a137cb61633ef574badff632b031925
inesp <inepiazzalonga@gmail.com>
2022-05-12 07:56:15 -0300

    promise en ItemListContainer

--- src/Components/ItemList/ItemList.jsx
+++ src/Components/ItemList/ItemList.jsx
@@ -1,65 +1,18 @@
-import React, { useEffect, useState } from 'react';
 import Item from "../Item/Item";
 import './ItemList.css';
-import Loader from "../../Assets/loader.gif"
-import productsList from "../../Data/csvjson.json"
 
 
-const ItemList = () => {
-  
-  const [products, setProducts] = useState([]);
-  const [loader, setLoader] = useState(true);
-  
-  const getData = new Promise ((res)=>{
-    setTimeout(()=>{
-      loader
-      res(productsList)
-    }, 3000)
-  })
-
-  useEffect(()=>{
-    getData
-    .then(res => setProducts(res))
-    .catch((err)=> console.log(err))
-    .finally(()=>setLoader(false))
-  }, [])
- 
+const ItemList = ({products}) => { 
 
   return (
-    <div className="product-list-container">
-      {
-        products.length ? ( 
-          <>
-            {
-              
-              products.map((product) => {
-           
-                return (
-                  <div key={product.id} className="itemInList">
-                    <Item
-                      img={product.img}
-                      name={product.name}
-                      thumbnail={product.thumbnail}
-                      price={product.price}
-                      stock={product.stock}
-                      id={product.id}
-                    />
-                  </div>
-                );
-              })
-            }
-          </>
-        ) :  (<div className='loaderInicio'>
-
-           <img src={Loader} className="loader"/>
-          <p className='loading'>Cargando productos...</p>
-        </div>
-       
+    <>
+      <div className="product-list-container">
+        {products.map( product => <Item key={product.id} id={product.id} name={product.name} img={product.img}/> ) }
+      </div>   
+                
      
-            
-        ) 
-      }
-    </div>
+    </>
+
   );
 };
 
--- src/Components/ItemListContainer/ItemListContainer.jsx
+++ src/Components/ItemListContainer/ItemListContainer.jsx
@@ -1,16 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import "./ItemListContainer.css"
 import ItemList from "../ItemList/ItemList"
+import productsList from "../../Data/csvjson.json"
+import Item from "../Item/Item"
+import Loader from "../../Assets/loader.gif"
+
+
 
 export default function ItemListContainer({greeting = "Shop"}) { 
-
+    const [products, setProducts] = useState([]);
+    const [loader, setLoader] = useState(true);
+    
+    const getData = new Promise ((res)=>{
+      setTimeout(()=>{
+        loader
+        res(productsList)
+      }, 3000)
+    })
+  
+    useEffect(()=>{
+      getData
+      .then(res => setProducts(res))
+      .catch((err)=> console.log(err))
+      .finally(()=>setLoader(false))
+    }, [])
 
     return (
+
         <>
         <h1 className="itemListContainer__title">{greeting}</h1>
         <div className="itemListContainer">
            
-            <ItemList />
+        {products.length > 0 ? <ItemList products={products}/> :  (<div className='loaderInicio'>
+        <img src={Loader} className="loader"/>
+        <p className='loading'>Cargando productos...</p> 
+      </div>)}         
         </div>
         </>
             
